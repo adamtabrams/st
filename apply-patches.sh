ee84d0d285e4c52e2901e9d4e89b2d964c733cb2
Adam Abrams <adam.abrams@optum.com>
2020-05-21 18:17:31 -0500

    add patches script

---
+++ apply-patches.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+patches="\
+    patches/st-scrollback-20200419-72e3f6c.diff
+    patches/st-scrollback-mouse-20191024-a2c479c.diff
+    patches/st-ligatures-scrollback-20200430-0.8.3.diff
+    patches/st-font2-20190416-ba72400.diff
+    patches/st-externalpipe-0.8.2.diff
+    patches/st-externalpipe-eternal-0.8.3.diff
+    patches/st-no_bold_colors-0.8.1.diff
+    patches/st-solarized-dark-20180411-041912a.diff
+"
+
+echo "$patches" | xargs -n 1 git apply --whitespace=fix
